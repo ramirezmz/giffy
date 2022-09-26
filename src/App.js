3bdd76f95351764fb73e31920d3bccb0ac24e7aa
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-26 19:07:43 -0300

    feat: using useState to the project and render our fist gif

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [value, updateValue]= useState(['https://media1.giphy.com/media/Q7SJReD3cqhql567za/giphy.gif?cid=ecf05e472qmixbzyppf65w1we36zjry6xbu7jq8x9137anm0&rid=giphy.gif&ct=g'])
+
   return (
     <div className="App">
       <section className="App-content">
-        gifs
+       <img src={value} />
       </section>
     </div>
   );

9f40a0663e5ead5faa6e6f57c8d29053fbc171fe
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-28 10:05:22 -0300

    add link to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import ListOfGifs from './components/ListOfGifs'
 
-import { Route } from 'wouter'
+import { Link, Route } from 'wouter'
 
 function App() {
 
@@ -10,6 +10,9 @@ function App() {
     <div className="App">
       <section className="App-content">
         <h1>App</h1>
+        <Link to='/gif/peru'>Gifs de Peru</Link>
+        <Link to='/gif/colombia'>Gifs de Colombia</Link>
+        <Link to='/gif/ecuador'>Gifs de Ecuador</Link>
         <Route 
           component={ListOfGifs}
           path="/gif/:keyword"

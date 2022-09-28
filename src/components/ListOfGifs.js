f17770b56229f031870f23cffa7e662874d56dbe
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-28 10:21:11 -0300

    add loading

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -3,13 +3,20 @@ import getGifs from '../services/getGifs';
 import Gif from './Gif'
 
 export default function ListOfGifs({params}) {
-  const { keyword } = params 
+  const { keyword } = params
+  const [loading, setLoading ] = useState(false)
   const [gifs, setGifs]= useState([])
 
   useEffect(function () {
+    setLoading(true)
     getGifs({ keyword })
-      .then(gifs => setGifs(gifs))
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
     }, [keyword])
+
+    if(loading) return <i>Cargando...</i>
 
   return <div>
   {gifs.map(({id, title, url}) =>

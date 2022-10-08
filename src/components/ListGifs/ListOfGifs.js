32a36eccae8a5466213b32a7a5a82cf2b88adfce
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-08 17:48:14 -0300

    separe in directories

---
+++ src/components/ListGifs/ListOfGifs.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import getGifs from '../../services/getGifs';
+import Gif from '../Gif/Gif'
+
+export default function ListOfGifs({params}) {
+  const { keyword } = params
+  const [loading, setLoading ] = useState(false)
+  const [gifs, setGifs]= useState([])
+
+  useEffect(function () {
+    setLoading(true)
+    getGifs({ keyword })
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
+    }, [keyword])
+
+    if(loading) return <i>Cargando...</i>
+
+  return <div>
+  {gifs.map(({id, title, url}) =>
+    <Gif 
+      id={id} 
+      key={id} 
+      title={title} 
+      url={url}
+    />)
+    }
+</div>
+}
